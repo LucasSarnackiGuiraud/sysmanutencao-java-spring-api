808b51a74276b951a894e3b8b5772a7ce3be977d
Lucas Sarnacki Guiraud <lucas.sguiraud@gmail.com>
2026-05-06 20:06:41 -0300

    Primeira versão da API Spring Boot

---
+++ src/main/java/com/lucas/sysmanutencao/SysmanutencaoApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.sysmanutencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysmanutencaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysmanutencaoApplication.class, args);
+	}
+
+}
